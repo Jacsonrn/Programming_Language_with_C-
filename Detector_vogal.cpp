13be22fdc1ab32ce5ea50b1736ff20102ac3f90e
Jacson Arruda Ribeiro <130600253+Jacsonrn@users.noreply.github.com>
2025-08-21 14:32:21 -0300

    Adiciona a questão para detectar vogais

---
+++ Detector_vogal.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char le;
+cout <<" Digite uma letra qualquer:"<<endl;
+cin >> le;
+if(le == 'a' || le == 'e' || le == 'i' || le == 'o' || le == 'u' ){
+    cout << "Voce digitou uma vogal minuscula."<< endl;
+    
+}
+return 0;
+}

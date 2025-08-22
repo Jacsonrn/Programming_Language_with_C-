bc77ce06b06c671dcfb181c1d4dc87510252bded
Jacson Arruda Ribeiro <130600253+Jacsonrn@users.noreply.github.com>
2025-08-22 14:27:01 -0300

    Utilização de Switch-Case

---
+++ Switchcase.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num = 0;
+    cout <<"Digite um numero:"<<endl;
+    cin>> num;
+    switch(num){
+        case 1:
+            cout <<"Voce escolheu a opcao 01."<<endl;
+            break;
+        case 2:
+            cout<< "Voce escolheu a opcao 02."<<endl;
+            break;
+          case 3:
+            cout<<"Voce escolheu a opcao 03."<<endl;
+            break;
+
+    }
+    return 0;
+}

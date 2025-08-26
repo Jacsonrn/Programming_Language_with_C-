071e27776f4677726cd2ee62ccbbdfd8be8440f9
Jacson Arruda Ribeiro <130600253+Jacsonrn@users.noreply.github.com>
2025-08-26 13:31:17 -0300

    Adiciona conceitos sobre For

---
+++ ComandoFor.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 5;
+    int i;
+    for(i = 10; a <= i ; i--){
+        cout <<"Vou estudar lip"<<endl;
+    }
+    return 0;
+}

7f56dc6144f31b1d53330bb50eefcb27a6150fe3
Jacson Arruda Ribeiro <130600253+Jacsonrn@users.noreply.github.com>
2025-09-09 15:39:47 -0300

    Exemplo laço While

---
+++ laçoWhile.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int numero;
+cout<<"Digite o numero de vezes:"<<endl;
+cin>>numero;
+
+    while(numero!=0){
+   cout<<"Vou estudar lip"<<endl;
+   cout<<"Digite um numero"<<endl;
+   cin>>numero;
+
+    }
+
+
+    return 0;
+}
